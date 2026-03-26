8780fc6a9c0aab4da72191c21182a2d06e9e67e2
Ha-med-xyz <hassan.rzahmed@outlook.com>
2026-03-25 22:26:00 -0400

    Add functionality to change button text by class

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Select every element with the toggle-button class
     const buttons = document.querySelectorAll('.toggle-button');
 
-    // Iterate over each button found
+    // Iterate over each button
     buttons.forEach(button => {
-        // Add click event listener to each button
+        // Add click event listener to buttons
         button.addEventListener('click', () => {
+            // Get the parent element project-card
+            const projectCard = button.closest('.project-card');
+            
+            // Find project-details element in the project card
+            const details = projectCard.querySelector('.project-details');
+            
+            // Toggle the active class on project-details element
+            details.classList.toggle('active');
+
+            // Change the button text if the active class is present
+            if (details.classList.contains('active')) {
+                button.textContent = 'Less...';
+            } else {
+                button.textContent = 'More...';
+            }
         });
     });
 });
