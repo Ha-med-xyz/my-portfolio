745a9fd1c5831f5d723f01e1dbea828212973950
Ha-med-xyz <hassan.rzahmed@outlook.com>
2026-03-25 23:43:00 -0400

    Rework form to include a submission button and add text box for messages

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         button.addEventListener('click', () => {
             // Get the parent element project-card
             const projectCard = button.closest('.project-card');
-            
+
             // Find project-details element in the project card
             const details = projectCard.querySelector('.project-details');
-            
+
             // Toggle the active class on project-details element
             details.classList.toggle('active');
 
@@ -47,3 +47,67 @@ function clearError(input, errorId) {
     input.classList.add('valid');
     document.getElementById(errorId).textContent = '';
 }
+
+// Validate name field (non-empty)
+function validateName() {
+    const value = nameInput.value.trim();
+    if (value === '') {
+        displayError(nameInput, 'name-error', 'Name cannot be empty.');
+        return false;
+    }
+    clearError(nameInput, 'name-error');
+    return true;
+}
+
+
+function validateEmail() {
+    const value = emailInput.value.trim();
+    // Basic email pattern: something@something.something
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (value === '') {
+        displayError(emailInput, 'email-error', 'Email cannot be empty.');
+        return false;
+    }
+    if (!emailPattern.test(value)) {
+        displayError(emailInput, 'email-error', 'Please enter a valid email address.');
+        return false;
+    }
+    clearError(emailInput, 'email-error');
+    return true;
+}
+
+function validateMessage() {
+    const value = msgInput.value.trim();
+    if (value === '') {
+        displayError(msgInput, 'message-error', 'Message cannot be empty.');
+        return false;
+    }
+    clearError(msgInput, 'message-error');
+    return true;
+}
+
+// Validate form submission
+form.addEventListener('submit', (event) => {
+// Prevent reload
+event.preventDefault;
+
+// Validate all
+const nameValid = validateName();
+const emailValid = validateEmail();
+const messageValid = validateMessage();
+
+// Continue if all fields are valid
+if (nameValid && emailValid && messageValid) {
+    successMsg.textContent = 'Message sent successfully.'
+
+    //Clear form 
+    form.reset();
+
+    // Clear styling from clearError
+    [nameInput, emailInput, msgInput].forEach(input => {
+            input.classList.remove('valid');
+        });
+    } else {
+        successMsg.textContent = '';
+    }
+});
