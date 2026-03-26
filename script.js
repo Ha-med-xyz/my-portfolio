2a18527ede346d1cb3ea76d9a34e2c50142aebb8
Ha-med-xyz <hassan.rzahmed@outlook.com>
2026-03-25 22:16:00 -0400

    Add event listener to buttons

---
+++ script.js
@@ -0,0 +1,12 @@
+// Wait for load before running the script
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Select every element with the toggle-button class
+    const buttons = document.querySelectorAll('.toggle-button');
+
+    // Iterate over each button found
+    buttons.forEach(button => {
+        // Add click event listener to each button
+        button.addEventListener('click', () => {
+        });
+    });
+});
